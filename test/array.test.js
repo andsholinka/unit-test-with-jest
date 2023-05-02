74f055acbb6451eea581c04ff89658fd1befe5fa
Andrey Sholinka <sholinka2111@gmail.com>
2023-05-02 09:25:11 +0700

    Array Matchers

---
+++ test/array.test.js
@@ -0,0 +1,18 @@
+test("array simple", () => {
+    const names = ["eko", "kurniawan", "khannedy"];
+    expect(names).toEqual(["eko", "kurniawan", "khannedy"]);
+    expect(names).toContain("eko");
+});
+
+test("array object", () => {
+    const persons = [{
+            name: "Eko"
+        },
+        {
+            name: "Budi"
+        }
+    ];
+    expect(persons).toContainEqual({
+        name: "Eko"
+    });
+});
